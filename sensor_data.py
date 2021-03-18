837732acccbf3e717529d84106d7cf40309fbcd2
AJ <ahensley968@gmail.com>
2021-03-17 19:11:08 -0600

    Modified Sensor Data to grab data from both the EC and PH sensor
    Printing values on seperate lines proves difficult to retrieve both values

--- sensor_data.py
+++ sensor_data.py
@@ -1,26 +1,41 @@
 import serial
 
-ser = serial.open('/dev/ttyUSB0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.flush()
 
 ph_level = ''
 ec_level = ''
 i = 0
 
-while True:
-    if ser.in_waiting > 0:
-        ph_level = ser.readline().decode('utf-8').rstrip()
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def get_data():
+    ph_level = ser.readline().decode('utf-8').rstrip()
+    ec_level = ser.readline().decode('utf-8').rstrip()
+    return ph_level, ec_level
 
 def current_ph(ph_sensor):
     if ph_sensor == 1:
-        return ph_level
+        ph_level, _ = get_data()
+        if is_number(ph_level):
+            return float(ph_level)
+        else:
+            return 0.0
     elif ph_sensor == 2:
         return 5.4
     else:
         return 0
 def current_ec(ec_sensor):
     if ec_sensor == 1:
-        return 1.0
+        if is_number(ec_level):
+            return float(ec_level)
+        else:
+            return 0.0
     elif ec_sensor == 2:
         return 1.5
     else:
