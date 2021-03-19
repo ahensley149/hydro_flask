f3baa0760e80807ed42adc0d3e06111ffc5bdfb8
AJ <ahensley968@gmail.com>
2021-03-18 20:26:21 -0600

    Switched serial names

--- sensor_data.py
+++ sensor_data.py
@@ -1,8 +1,8 @@
 import serial
 import re
 
-uno = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
-nano = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+uno = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+nano = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 uno.flush()
 nano.flush()
 
