3358aaef6d1c96da506ac0103f6895d35f05d4ba
AJ <ahensley968@gmail.com>
2021-05-20 19:36:31 -0600

    Updated airtemp with Webhook triggers and MQTT communication to control the door

--- air_temp.py
+++ air_temp.py
@@ -2,13 +2,19 @@
 
 import json
 import time
+import requests
+import paho.mqtt.client as mqtt
 from urllib.request import urlopen
 
+
+#client = mqtt.Client('door')
+#client.connect('localhost')
 
 min_humid = 50
 max_humid = 70
 
 weather_forecast = 'https://api.weather.gov/gridpoints/BOU/58,92/forecast/hourly'
+weather_station = '#'
 
 def get_weather_forecast():
     """Retrieves NWS hourly forecast for the location of the grow room
@@ -23,6 +29,21 @@ def get_weather_forecast():
         forecast_temps.append(hourly_forecasts[i]['temperature'])
 
     return forecast_temps
+
+def get_outdoor_temp():
+    """Retrieves weather data from tempest weather station in Backyard
+    """
+    response = urlopen(weather_station)
+    data = json.loads(response.read().decode())
+    outdoor_temp = (data['obs'][0]['air_temperature'] * 1.8) + 32
+    return int(outdoor_temp)
+
+def get_hygrometer_reading():
+    """Retrieves the Inside temp and humidity from the Ubibot Rest API for the hygrometer in the grow room
+    """
+    inside_temp = 69
+    inside_humid = 60
+    return inside_temp, inside_humid
 
 def get_temp_limits(outdoor_temp):
     """Set temperature limits based on outside temps and forecasts
@@ -54,36 +75,53 @@ def get_temp_limits(outdoor_temp):
 
     return min_temp, max_temp
 
-def check_air(inside_temp, outdoor_temp, inside_humid, outdoor_humid, min_temp, max_temp):
+def check_air(inside_temp, outdoor_temp, inside_humid, min_temp, max_temp):
     """Makes a decision on actions to take depeneding on the temperature and humidity 
       both inside and outside of the grow room
     """
+    temp_range = max_temp - min_temp
     if inside_temp <= min_temp:
-        if outdoor_temp - 15 > inside_temp:
-            #Open door and turn on bottom fan
-            pass
+        if outdoor_temp - 10 > inside_temp:
+            #client.publish("/door/openclose","2")
+            r = requests.get(url = "https://maker.ifttt.com/trigger/bottom_fan_on/with/key/beLHb8ZjZAJVzHxB1nSatB")
         else:
-            print('Turn on heater')
+            r = requests.get(url = "https://maker.ifttt.com/trigger/heater_on/with/key/beLHb8ZjZAJVzHxB1nSatB")
         if inside_humid <= min_humid:
             print('Turn on humidifier')
     elif inside_temp >= max_temp:
-        #Crack door, turn on swamp cooler
+        #client.publish("/door/openclose","1")
+        r = requests.get(url = "https://maker.ifttt.com/trigger/swamp_cooler_on/with/key/beLHb8ZjZAJVzHxB1nSatB")
         if inside_temp >= max_temp + 2:
-            #Turn on upper vent fan
-            pass
+            r = requests.get(url = "https://maker.ifttt.com/trigger/upper_fan_on/with/key/beLHb8ZjZAJVzHxB1nSatB")
         if inside_temp >= max_temp + 4:
             #Turn on lower vent fan and cross wind fan
-            pass
+            r = requests.get(url = "https://maker.ifttt.com/trigger/bottom_fan_on/with/key/beLHb8ZjZAJVzHxB1nSatB")
         if inside_humid <= min_humid:
             #Turn on humidifier
             pass
         if inside_humid >= max_humid:
             #Turn off swamp cooler and humidifier, make sure door is open
-            pass
+            #client.publish("/door/openclose","2")
+            r = requests.get(url = "https://maker.ifttt.com/trigger/swamp_cooler_off/with/key/beLHb8ZjZAJVzHxB1nSatB")
+    elif inside_temp - (temp_range/3) > min_temp and inside_temp + (temp_range/3) < max_temp:
+        r = requests.get(url = "https://maker.ifttt.com/trigger/swamp_cooler_off/with/key/beLHb8ZjZAJVzHxB1nSatB")
+        r = requests.get(url = "https://maker.ifttt.com/trigger/heater_off/with/key/beLHb8ZjZAJVzHxB1nSatB")
+        r = requests.get(url = "https://maker.ifttt.com/trigger/bottom_fan_off/with/key/beLHb8ZjZAJVzHxB1nSatB")
+        r = requests.get(url = "https://maker.ifttt.com/trigger/upper_fan_off/with/key/beLHb8ZjZAJVzHxB1nSatB")
+        #client.publish("/door/openclose", "0")
     else:
         if inside_humid <= min_humid:
             print('Turn on humidifier')
         elif inside_humid >= max_humid:
             #Turn off humidifer and or swamp cooler and open door
+            #client.publish("/door/openclose", "3")
             print('Open door')
             
+forecast = get_weather_forecast()
+outdoor_temp = get_outdoor_temp()
+min_temp, max_temp = get_temp_limits(outdoor_temp)
+inside_temp, inside_humid = get_hygrometer_reading()
+
+print(forecast)
+
+#check_air(inside_temp, outdoor_temp, inside_humid, min_temp, max_temp)
